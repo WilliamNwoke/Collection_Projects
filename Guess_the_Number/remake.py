c5bea87b6efec1738087c5f38cfbd4f002e96c7b
will <willteey@gmail.com>
2021-11-25 17:50:40 -0800

    SU_labs

--- Guess_the_Number/remake.py
+++ Guess_the_Number/remake.py
@@ -1,22 +1,24 @@
-# Retouching the initial Program xDDDD
 import random
 
 def guess(x):
     random_number = random.randint(1, x)
 
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}: '))
-        if guess > random_number:
-            print('Your guess is too high')
-        
+    while True:
+        guess = int(input(f'Guess the number between 1 and {x}: '))
         if guess < random_number:
-            print('Your guess is too low')
+            print('Sorry, guess again. Too low.')
+        
+        if guess > random_number:
+            print('Sorry, guess again. Too high.')
+            
+        if guess == random_number:
+            print(f'Yay, congrats. You have guessed the number {random_number}')
+            break
 
-    print(f'Yay you guessed the random number : {random_number} ')
-
-print ('Guess the random number game')
-print()
-x = int(input('Pick a range betwen 1 and your number, What is your number: '))
+        
+print('-------------------------------------')
+print('\tGuessing Game')
+print('-------------------------------------\n\n')
+x = int(input('Pick a range between 1 and your number, what is your number: '))
 print()
 guess(x)
---
+++ Madlib/madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because\
+ love to {verb1}. Stay hydrated to {verb2} like you are {famous_person}"
+
+print(madlib)
