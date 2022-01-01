ba8b636da1f32c4de72770d9342aae5db0a1ba7d
William <willteey@gmail.com>
2021-12-31 21:33:24 -0800

    Windows Wifi finder

---
+++ Wifi_password_finder/README.md
@@ -0,0 +1,11 @@
+### Windows Wifi Finder
+
+A python application to run the commands
+
+The command displays list of know networks
+
+> netsh wlan show profiles
+
+The second command iterates through the list of command 1 and prints out the password
+
+> netsh wlan show profiles list[i] key='clear'
---
+++ Wifi_password_finder/main.py
@@ -0,0 +1,17 @@
+import subprocess
+
+# Will store the profiles data
+data = subprocess.check_output(['netsh', 'wlan','show','profiles']).decode('utf-8').split('\n')
+profiles = [i.split(':')[1][1:-1] for i in data if 'All User Profile' in i]
+
+# We wil check and print the passwords if they are available
+for i in profiles:
+    # Command to check for passwords
+    results= subprocess.check_output(['netsh', 'wlan','show','profiles', i, 'key=clear']).decode('utf-8').split('\n')
+    # Storing passwords after converting to list
+    results = [b.split(':')[1][1:-1] for b in results if 'Key Content' in b]
+    # printing the wifi-names and passwords
+    try:
+        print ("{:<30}|   {:<}".format(i, results[0]))
+    except IndexError:
+        print ("{:<30}|   {:<}".format(i, ""))
